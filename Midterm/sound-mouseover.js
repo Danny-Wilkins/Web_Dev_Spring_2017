898bb1a2db0ebf2f6fa866d3a836015e0a665b8b
Danny Wilkins <daniel.wilkins@nyu.edu>
2017-03-19 16:38:44 -0400

    NOW WITH 400% MORE GLORIOUS AUDIO.

--- Midterm/sound-mouseover.js
+++ Midterm/sound-mouseover.js
@@ -1,6 +1,27 @@
-function playclip() {
+function playclip1() {
 
-    var audio = document.getElementsByTagName("audio")[0];
+    var audio = document.getElementById("break");
+    audio.play();
+
+}
+
+function playclip2() {
+
+    var audio = document.getElementById("stop");
+    audio.play();
+
+}
+
+function playclip3() {
+
+    var audio = document.getElementById("words");
+    audio.play();
+
+}
+
+function playclip4() {
+
+    var audio = document.getElementById("say");
     audio.play();
 
 }
