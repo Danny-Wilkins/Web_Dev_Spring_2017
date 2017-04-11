c50841db1d68571aab6a6ed86458edda77104d29
Danny Wilkins <daniel.wilkins@nyu.edu>
2017-04-11 15:03:32 -0400

    Oops

--- Week_9/homework.js
+++ Week_9/homework.js
@@ -1,9 +1,6 @@
 function twenty_sided_die()
 {
-	for(var i = 0; i < 100; i++)
-	{
-		alert(Math.floor(Math.random() * 20) + 1);
-	}
+	alert(Math.floor(Math.random() * 20) + 1);
 }
 
 function hundred_times()
