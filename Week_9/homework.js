dc831e16b34ea1eef661c1bf8b326c08d7531ff0
Danny Wilkins <daniel.wilkins@nyu.edu>
2017-04-11 15:00:12 -0400

    Homework for week 9, a nice 20-sided die roller in JavaScript.

---
+++ Week_9/homework.js
@@ -0,0 +1,15 @@
+function twenty_sided_die()
+{
+	for(var i = 0; i < 100; i++)
+	{
+		alert(Math.floor(Math.random() * 20) + 1);
+	}
+}
+
+function hundred_times()
+{
+	for(var i = 0; i < 100; i++)
+	{
+		alert(Math.floor(Math.random() * 20) + 1);
+	}
+}
